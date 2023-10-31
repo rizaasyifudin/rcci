4b21d30e65424ba8b74e04f03d8e3937f271ebd0
Riza As <54466467+rizaasyifudin@users.noreply.github.com>
2023-10-31 15:37:16 +0700

    Update BaseModelCustom.php

--- BaseModelCustom.php
+++ BaseModelCustom.php
@@ -9,7 +9,7 @@
  * the LICENSE file that was distributed with this source code.
  */
 
-namespace App\Libraries\ModelCustom;
+namespace CodeIgniter;
 
 use Closure;
 use CodeIgniter\Database\BaseConnection;
